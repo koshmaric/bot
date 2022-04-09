509e2d67d8c2b2dab8e34b21d065f75a00a6af5c
koshmaric <koshmaric1tvoi2strax@gmail.com>
2022-04-09 19:47:05 +0300

    update

---
+++ text.py
@@ -0,0 +1,105 @@
+import random
+help_text = """
+.spam 1 2 - Где 1 это число отправлений, а 2 это текст (Обычная отправка сообщений с указаным текстом)
+.spamg 1 - Времено недоступны
+.spams 1 - Времено недоступно
+.type 2 - 2 это текст (Красиво печатает текст)
+.gul - Ты становишься гулем
+.love - анимация сердечка
+"""
+#{random.choice(heart)}
+heart = ['💙', '💚', '❤️', '💜', '🧡', '💛']
+heart_ani_2 = f"""
+🤍🤍🖤🖤🖤🤍🖤🖤🖤🤍
+🤍🖤{random.choice(heart)}{random.choice(heart)}{random.choice(heart)}🖤{random.choice(heart)}{random.choice(heart)}{random.choice(heart)}🖤
+🖤{random.choice(heart)}{random.choice(heart)}{random.choice(heart)}{random.choice(heart)}{random.choice(heart)}{random.choice(heart)}{random.choice(heart)}{random.choice(heart)}{random.choice(heart)}
+🖤{random.choice(heart)}{random.choice(heart)}{random.choice(heart)}{random.choice(heart)}{random.choice(heart)}{random.choice(heart)}{random.choice(heart)}{random.choice(heart)}{random.choice(heart)}
+🖤{random.choice(heart)}{random.choice(heart)}{random.choice(heart)}{random.choice(heart)}{random.choice(heart)}{random.choice(heart)}{random.choice(heart)}{random.choice(heart)}{random.choice(heart)}
+🖤{random.choice(heart)}{random.choice(heart)}{random.choice(heart)}{random.choice(heart)}{random.choice(heart)}{random.choice(heart)}{random.choice(heart)}{random.choice(heart)}{random.choice(heart)}
+🤍🖤{random.choice(heart)}{random.choice(heart)}{random.choice(heart)}{random.choice(heart)}{random.choice(heart)}{random.choice(heart)}{random.choice(heart)}🖤
+🤍🤍🖤{random.choice(heart)}{random.choice(heart)}{random.choice(heart)}{random.choice(heart)}{random.choice(heart)}🖤🤍
+🤍🤍🤍🖤{random.choice(heart)}{random.choice(heart)}{random.choice(heart)}🖤🤍🤍
+🤍🤍🤍🤍🖤{random.choice(heart)}🖤🤍🤍🤍
+"""
+heart_ani_3 = f"""
+🤍🤍🖤🖤🖤🤍🖤🖤🖤
+🤍🖤{random.choice(heart)}{random.choice(heart)}{random.choice(heart)}🖤{random.choice(heart)}{random.choice(heart)}{random.choice(heart)}
+🖤{random.choice(heart)}{random.choice(heart)}{random.choice(heart)}{random.choice(heart)}{random.choice(heart)}{random.choice(heart)}{random.choice(heart)}{random.choice(heart)}
+🖤{random.choice(heart)}{random.choice(heart)}{random.choice(heart)}{random.choice(heart)}{random.choice(heart)}{random.choice(heart)}{random.choice(heart)}{random.choice(heart)}
+🖤{random.choice(heart)}{random.choice(heart)}{random.choice(heart)}{random.choice(heart)}{random.choice(heart)}{random.choice(heart)}{random.choice(heart)}{random.choice(heart)}
+🖤{random.choice(heart)}{random.choice(heart)}{random.choice(heart)}{random.choice(heart)}{random.choice(heart)}{random.choice(heart)}{random.choice(heart)}{random.choice(heart)}
+🤍🖤{random.choice(heart)}{random.choice(heart)}{random.choice(heart)}{random.choice(heart)}{random.choice(heart)}{random.choice(heart)}{random.choice(heart)}
+🤍🤍🖤{random.choice(heart)}{random.choice(heart)}{random.choice(heart)}{random.choice(heart)}{random.choice(heart)}🖤
+🤍🤍🤍🖤{random.choice(heart)}{random.choice(heart)}{random.choice(heart)}🖤🤍
+"""
+heart_ani_4 = f"""
+🤍🤍🖤🖤🖤🤍🖤🖤
+🤍🖤{random.choice(heart)}{random.choice(heart)}{random.choice(heart)}🖤{random.choice(heart)}{random.choice(heart)}
+🖤{random.choice(heart)}{random.choice(heart)}{random.choice(heart)}{random.choice(heart)}{random.choice(heart)}{random.choice(heart)}{random.choice(heart)}
+🖤{random.choice(heart)}{random.choice(heart)}{random.choice(heart)}{random.choice(heart)}{random.choice(heart)}{random.choice(heart)}{random.choice(heart)}
+🖤{random.choice(heart)}{random.choice(heart)}{random.choice(heart)}{random.choice(heart)}{random.choice(heart)}{random.choice(heart)}{random.choice(heart)}
+🖤{random.choice(heart)}{random.choice(heart)}{random.choice(heart)}{random.choice(heart)}{random.choice(heart)}{random.choice(heart)}{random.choice(heart)}
+🤍🖤{random.choice(heart)}{random.choice(heart)}{random.choice(heart)}{random.choice(heart)}{random.choice(heart)}{random.choice(heart)}
+🤍🤍🖤{random.choice(heart)}{random.choice(heart)}{random.choice(heart)}{random.choice(heart)}{random.choice(heart)}
+"""
+heart_ani_5 = f"""
+🤍🤍🖤🖤🖤🤍🖤
+🤍🖤{random.choice(heart)}{random.choice(heart)}{random.choice(heart)}🖤{random.choice(heart)}
+🖤{random.choice(heart)}{random.choice(heart)}{random.choice(heart)}{random.choice(heart)}{random.choice(heart)}{random.choice(heart)}
+🖤{random.choice(heart)}{random.choice(heart)}{random.choice(heart)}{random.choice(heart)}{random.choice(heart)}{random.choice(heart)}
+🖤{random.choice(heart)}{random.choice(heart)}{random.choice(heart)}{random.choice(heart)}{random.choice(heart)}{random.choice(heart)}
+🖤{random.choice(heart)}{random.choice(heart)}{random.choice(heart)}{random.choice(heart)}{random.choice(heart)}{random.choice(heart)}
+🤍🖤{random.choice(heart)}{random.choice(heart)}{random.choice(heart)}{random.choice(heart)}{random.choice(heart)}
+"""
+heart_ani_6 = f"""
+🤍🤍🖤🖤🖤🤍
+🤍🖤{random.choice(heart)}{random.choice(heart)}{random.choice(heart)}🖤
+🖤{random.choice(heart)}{random.choice(heart)}{random.choice(heart)}{random.choice(heart)}{random.choice(heart)}
+🖤{random.choice(heart)}{random.choice(heart)}{random.choice(heart)}{random.choice(heart)}{random.choice(heart)}
+🖤{random.choice(heart)}{random.choice(heart)}{random.choice(heart)}{random.choice(heart)}{random.choice(heart)}
+🖤{random.choice(heart)}{random.choice(heart)}{random.choice(heart)}{random.choice(heart)}{random.choice(heart)}
+"""
+heart_ani_7 = f"""
+🤍🤍🖤🖤🖤
+🤍🖤{random.choice(heart)}{random.choice(heart)}{random.choice(heart)}
+🖤{random.choice(heart)}{random.choice(heart)}{random.choice(heart)}{random.choice(heart)}
+🖤{random.choice(heart)}{random.choice(heart)}{random.choice(heart)}{random.choice(heart)}
+🖤{random.choice(heart)}{random.choice(heart)}{random.choice(heart)}{random.choice(heart)}
+"""
+heart_ani_8 = f"""
+🤍🤍🖤🖤
+🤍🖤{random.choice(heart)}{random.choice(heart)}
+🖤{random.choice(heart)}{random.choice(heart)}{random.choice(heart)}
+🖤{random.choice(heart)}{random.choice(heart)}{random.choice(heart)}
+"""
+heart_ani_9 = f"""
+🤍🤍🖤
+🤍🖤{random.choice(heart)}
+🖤{random.choice(heart)}{random.choice(heart)}
+"""
+heart_ani_10 = f"""
+🤍🤍
+🤍🖤
+"""
+heart_ani_11 = f"""
+🤍⠀
+"""
+heart_ani_12 = f"""
+🤍I love you
+"""
+
+def heart_ani():
+    heart_ani_1 = f"""
+🤍🤍🖤🖤🖤🤍🖤🖤🖤🤍🤍
+🤍🖤{random.choice(heart)}{random.choice(heart)}{random.choice(heart)}🖤{random.choice(heart)}{random.choice(heart)}{random.choice(heart)}🖤🤍
+🖤{random.choice(heart)}{random.choice(heart)}{random.choice(heart)}{random.choice(heart)}{random.choice(heart)}{random.choice(heart)}{random.choice(heart)}{random.choice(heart)}{random.choice(heart)}🖤
+🖤{random.choice(heart)}{random.choice(heart)}{random.choice(heart)}{random.choice(heart)}{random.choice(heart)}{random.choice(heart)}{random.choice(heart)}{random.choice(heart)}{random.choice(heart)}🖤
+🖤{random.choice(heart)}{random.choice(heart)}{random.choice(heart)}{random.choice(heart)}{random.choice(heart)}{random.choice(heart)}{random.choice(heart)}{random.choice(heart)}{random.choice(heart)}🖤
+🖤{random.choice(heart)}{random.choice(heart)}{random.choice(heart)}{random.choice(heart)}{random.choice(heart)}{random.choice(heart)}{random.choice(heart)}{random.choice(heart)}{random.choice(heart)}🖤
+🤍🖤{random.choice(heart)}{random.choice(heart)}{random.choice(heart)}{random.choice(heart)}{random.choice(heart)}{random.choice(heart)}{random.choice(heart)}🖤🤍
+🤍🤍🖤{random.choice(heart)}{random.choice(heart)}{random.choice(heart)}{random.choice(heart)}{random.choice(heart)}🖤🤍🤍
+🤍🤍🤍🖤{random.choice(heart)}{random.choice(heart)}{random.choice(heart)}🖤🤍🤍🤍
+🤍🤍🤍🤍🖤{random.choice(heart)}🖤🤍🤍🤍🤍
+🤍🤍🤍🤍🤍🖤🤍🤍🤍🤍🤍
+"""
+    return heart_ani_1
